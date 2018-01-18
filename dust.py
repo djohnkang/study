ba226614775046185a76e452f8b3ed2dce38c966
biniprc <biniprc@hotmail.com>
2018-01-18 15:41:47 +0900

    2

---
+++ dust.py
@@ -0,0 +1,18 @@
+# 실습1. 미세먼지 정보 활용하기
+import requests
+url = "http://openapi.seoul.go.kr:8088/54775266536e6564393578574d4c41/json/TimeAverageAirQuality/1/25/201801171700"
+response_body = requests.get(url).json()
+
+dust = int(response_body['TimeAverageAirQuality']['row'][0]['PM10'])
+
+# 1. dust 변수에 담겨 있는 미세먼지 농도 값 출력하기
+print(dust)
+# 2. 조건문을 활용하여 미세먼지 농도에 따른 미세먼지 상태 출력하기
+if 150 < dust:
+  print("매우나쁨")
+elif 80 < dust:
+  print("나쁨")
+elif 30 < dust:
+  print("보통")
+else:
+  print("좋음")
